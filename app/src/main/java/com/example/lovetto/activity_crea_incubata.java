56de32741986c8de25952d3f677af9795eb080ac
Simona Grieco <simonagrieco98@gmail.com>
2020-07-07 23:05:03 +0200

    creata pagina home

---
+++ app/src/main/java/com/example/lovetto/activity_crea_incubata.java
@@ -0,0 +1,14 @@
+package com.example.lovetto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_crea_incubata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crea_incubata);
+    }
+}
---
+++ app/src/main/java/com/example/lovetto/activity_dettagli_incubata.java
@@ -0,0 +1,14 @@
+package com.example.lovetto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activity_dettagli_incubata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dettagli_incubata);
+    }
+}
