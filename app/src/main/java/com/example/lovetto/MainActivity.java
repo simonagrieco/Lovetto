1ad570b459ccf985f4e699bb04ea0f93a12464a6
Simona Grieco <simonagrieco98@gmail.com>
2020-07-09 00:23:36 +0200

    creata pagina "crea incubata" e "dettagli incubata"

--- app/src/main/java/com/example/lovetto/MainActivity.java
+++ app/src/main/java/com/example/lovetto/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.lovetto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lanciaDettagliIncubata(View view) {
+        intent = new Intent(this, activity_dettagli_incubata.class);
+        startActivity(intent);
+    }
+
+    public void lanciaCreaIncubata(View view) {
+        intent = new Intent(this, activity_crea_incubata.class);
+        startActivity(intent);
     }
 }
