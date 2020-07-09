6999d8d04d45203937ca35f738b6afd176cfbd52
Simona Grieco <simonagrieco98@gmail.com>
2020-07-09 20:01:22 +0200

    creata pagina "modofica dettagli incubata" e vari miglioramenti

--- app/src/main/java/com/example/lovetto/activity_crea_incubata.java
+++ app/src/main/java/com/example/lovetto/activity_crea_incubata.java
@@ -1,14 +1,23 @@
 package com.example.lovetto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_crea_incubata extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crea_incubata);
+    }
+
+
+    public void lanciaDettagliIncubatore(View view) {
+        intent = new Intent(this, activity_dettagli_incubatore.class);
+        startActivity(intent);
     }
 }
