387c27e2056f13b5654fb0977d17f92002afe703
Gianluca Rossi <Qlett@icloud.com>
2020-07-12 05:23:56 +0200

    Aggiunta pagina di controllo remoto, animazioni, icone, stile
    1. Pagina di controllo remoto:
    - scritta da 0, mi ci sono voluti 2 giorni, programmare per Android è un incubo
    
    2. Animazioni:
    - creata cartella anim
    - creati file di animazione relativi alla schermata di controllo remoto
    
    3. Icone:
    - aggiunte quelle relative a controllo remoto
    
    4. styles.xml, strings.xml, dimens.xml, colors.xml:
    - aggiunti nuovi valori

--- app/src/main/java/com/example/lovetto/activity_dettagli_incubata.java
+++ app/src/main/java/com/example/lovetto/activity_dettagli_incubata.java
@@ -1,14 +1,22 @@
 package com.example.lovetto;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class activity_dettagli_incubata extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dettagli_incubata);
+    }
+
+    public void mostraControlloRemoto(View view) {
+        intent = new Intent(this, activity_controllo_remoto.class);
+        startActivity(intent);
     }
 }
