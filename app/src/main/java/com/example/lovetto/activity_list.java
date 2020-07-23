b0b0221e608f2c7e4c8f468b17e5248304e35ca0
Simona Grieco <simonagrieco98@gmail.com>
2020-07-23 02:21:52 +0200

    creata pagina "lista incubate" e gestita scelta tipologia uova in "crea incubata"

---
+++ app/src/main/java/com/example/lovetto/activity_list.java
@@ -0,0 +1,14 @@
+package com.example.lovetto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
